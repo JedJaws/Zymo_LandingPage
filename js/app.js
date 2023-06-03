72be534dfe8f131c80197948cd7ca066a5dccbca
Christian Verry <CJVerry@csu.fullerton.edu>
2023-06-02 20:26:51 -0700

    submit success function in progress

--- js/app.js
+++ js/app.js
@@ -4,3 +4,11 @@ function toBottom(){
     document.body.scrollIntoView(false);
     document.documentElement.scrollHeight(false);
 }
+
+const form = document.querySelector('form');
+const submitSuccess = document.querySelector('#submit_successful');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    submitSuccess.classList.add('show');
+    setTimeout(() => form.submit(), 2000);
+});
